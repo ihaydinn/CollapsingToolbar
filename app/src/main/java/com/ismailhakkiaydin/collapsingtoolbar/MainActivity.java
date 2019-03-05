3d94101d38089ce0b09ec9d8a22d00d198918b52
Ismail Hakki Aydin <ihaydin7@gmail.com>
2019-03-06 02:01:32 +0300

    Collapsing Toolbar

---
+++ app/src/main/java/com/ismailhakkiaydin/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ismailhakkiaydin.collapsingtoolbar;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("CollapsingToolbarLayout");
+        toolbar.inflateMenu(R.menu.menu_main);
+
+        CollapsingToolbarLayout collapsingToolbarLayout= (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+        collapsingToolbarLayout.setTitle("Yeni Başlık");
+    }
+}
